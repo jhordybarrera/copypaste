1a194b2d0cf44ac9dd2898112189ce7c873a1193
Jhordy Barrera <jbarreraballestas@gmail.com>
2025-12-17 13:50:05 -0500

    proxy reverse forward

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if(request()->header('x-forwarded-host') != null){
+            URL::forceScheme('https');
+            URL::forceRootURL('https://'.request()->header('x-forwarded-host'));
+        }
     }
 
     /**
